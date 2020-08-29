81cf483baf877b0d723bb547f732e26e98655c89
Hannah <Hannah>
2020-08-29 22:48:53 +0800

    2

--- Fighting/test4/test4.js
+++ Fighting/test4/test4.js
@@ -2,7 +2,7 @@ var Scores;
 function piPei() {
     let Number = document.getElementById("Score").value;
     let dengji = parseInt(Number / 10);
-    // 方法一switch1
+    // 方法一switch12
     switch (dengji) {
         case 10:
         case 9:
